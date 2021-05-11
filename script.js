4dfdc2f95bbe6cda8f8d5846f3c9497a8c6c4afb
ultrasoft011 <ultrasoft.011@gmail.com>
2021-05-10 21:52:14 -0500

    Ajax process

--- script.js
+++ script.js
@@ -2,5 +2,11 @@
 
 // AJAX Call
 
-const request = new XMLHttpRequest();
-const url = rquest.open('GET', '')
+const request = new XMLHttpRequest(); // New XMLHttpRequest
+request.open('GET', 'https://restcountries.eu/rest/v2/name/colombia'); // URL API
+request.send(); // Send the request 
+
+request.addEventListener('load', function () { // Load event, waiting for that event and as soon as arrive the function will be executed
+    console.log(this.responseText);
+
+})
