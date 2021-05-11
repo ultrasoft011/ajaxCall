1897ceaab393ac352124f8e6c9a8bdc014c67c6f
ultrasoft011 <ultrasoft.011@gmail.com>
2021-05-11 12:16:03 -0500

    destructuring

---
+++ script1.js
@@ -0,0 +1,34 @@
+'use strict' ; 
+
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+};
+
+let [main, secondary] = restaurant.categories;
+[main, secondary] = [secondary, main]
+console.log(main, secondary);
+
+
+const { name } = restaurant;
+const {
+    name: Monodeia
+} = restaurant;
+console.log(Monodeia);
+
+// const [first, , third] = restaurant.categories;
+// console.log(first, third);
+
+// Array Destructuring ESX
+
+// const arr = [2, 3, 4];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
+
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+
